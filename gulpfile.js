c870e2fcbef4fc9db10fe6e048a69c908d4ab800
Yacine Rezgui <rezgui.y@gmail.com>
2014-11-24 10:13:04 +0000

    Add NPM packages & Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp        = require('gulp');
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream');
+var concat      = require('gulp-concat');
+var reactify    = require('reactify');
+
+gulp.task('script', function() {
+
+  var b = browserify({ debug: true });
+  b.add('./src/scripts/app.react.js');
+
+  return b
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('style', function () {
+  return gulp.src('src/styles/main.css')
+             .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('template', function() {
+  gulp.src('src/index.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['script', 'style', 'template']);
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch('src/**/*.html', ['template']);
+  gulp.watch('src/**/*.css',  ['style']);
+  gulp.watch('src/**/*.js',   ['script']);
+});
