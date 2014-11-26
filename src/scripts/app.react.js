d295ca975fc3a90684ccf8b97327618cea9472d3
Yacine Rezgui <rezgui.y@gmail.com>
2014-11-26 17:27:09 +0000

    Simplify render function by separating styling functions into component methods

--- src/scripts/app.react.js
+++ src/scripts/app.react.js
@@ -7,6 +7,7 @@ var options = {
 var ReactDataTable = React.createClass({
   getInitialState: function() {
     return {
+      tableHeight: this.calculateTableHeight(),
       rowsToDisplay: {
         toHideAbove: 0,
         toRender: 0,
@@ -14,64 +15,47 @@ var ReactDataTable = React.createClass({
       }
     };
   },
-  render: function() {
-    var createCell = function createCell (key, value) {
-      return <td key={key}>{value}</td>;
-    };
+  componentDidMount: function(elem) {
+    this.decideRowsToDisplay();
+    window.addEventListener('scroll', this.decideRowsToDisplay);
+  },
+  componentDidUnmount: function(elem) {
+    window.removeEventListener('scroll', this.decideRowsToDisplay);
+  },
+  createCell: function createCell (key, value) {
+    return <td key={key}>{value}</td>;
+  },
+  createRow: function createRow (row) {
+    return (
+      <tr key={row._id}>{
+        Object.keys(row).map(function(key) {
+          return this.createCell(key, row[key]);
+        }.bind(this))
+      }</tr>
+    );
+  },
+  createHeaderCell: function createHeaderCell (key) {
+    return <th key={key}>{key}</th>;
+  },
+  filterRowsToBeRendered: function filterRowsToBeRendered (table) {
+    table = this.props.data.slice(this.state.rowsToDisplay.toHideAbove);
 
-    var createRow = function createRow (row) {
-      return (
-        <tr key={row._id}>{
-          Object.keys(row).map( function(key) {
-            return createCell(key, row[key]);
-          })
-        }</tr>
-      );
-    };
-
-    var createHeaderCell = function createHeaderCell (key) {
-      return <th key={key}>{key}</th>;
-    };
-
-    var createTable = function createTable (origTable) {
-      if (this.isMounted()) {
-        var table = origTable.slice(this.state.rowsToDisplay.toHideAbove);
-        if (this.state.rowsToDisplay.toHideBelow) {
-          table = table.slice(0,-this.state.rowsToDisplay.toHideBelow);
-        }
-        var paddingTop = this.state.rowsToDisplay.toHideAbove * this.props.options.rowHeight;
-        var paddingBottom = this.state.rowsToDisplay.toHideBelow * this.props.options.rowHeight;
-        var tableElement = this.refs.table.getDOMNode();
-        tableElement.style.paddingTop = paddingTop +'px';
-        tableElement.style.paddingBottom = paddingBottom +'px'
-      } else {
-        var table = origTable.map(function () {
-          return {}
-        });
-      }
-      return (
-        <table ref="table">
-
-          <thead>{
-            Object.keys(table[0]).map(function (key) {
-              return createHeaderCell(key);
-            })
-          }</thead>
-          <tbody>
-            {table.map(createRow)}
-          </tbody>
-        </table>
-      );
-    }.bind(this);
-
-    return createTable(this.props.data);
+    if (this.state.rowsToDisplay.toHideBelow) {
+      table = table.slice(0,-this.state.rowsToDisplay.toHideBelow);
+    }
+    
+    return table;
+  },
+  calculateTableHeight: function () {
+    return this.props.data.length * this.props.options.rowHeight;
   },
   decideRowsToDisplay: function () {
-    var scrollTop = window.pageYOffset;
-    var viewportHeight = window.innerHeight;
-    var numRows = this.props.data.length;
+    var scrollTop       = window.pageYOffset;
+    var viewportHeight  = window.innerHeight;
+    var numRows         = this.props.data.length;
+
     // TODO: don't assume that the component is the only element in the page
-    var renderTop = Math.max(scrollTop - viewportHeight, 0);
+    var renderTop       = Math.max(scrollTop - viewportHeight, 0);
     var rowRenderOffset = Math.floor(renderTop / this.props.options.rowHeight);
     var maxRowsToRender = Math.ceil(viewportHeight * 3 / this.props.options.rowHeight);
     var numRowsToRender = Math.min(numRows - rowRenderOffset, maxRowsToRender);
@@ -84,15 +68,39 @@ var ReactDataTable = React.createClass({
       }
     })
   },
-  componentDidMount: function(elem) {
-    this.decideRowsToDisplay();
-    window.addEventListener('scroll', function() {
-      // TODO: add rate limit
-      this.decideRowsToDisplay();
-    }.bind(this));
-  },
-  componentDidUnmount: function(elem) {
-    window.removeEventListener('scroll');
+  render: function() {
+    if (!this.isMounted()) {
+      return <div style={{height: this.state.tableHeight}} />
+    }
+
+    var table                 = this.filterRowsToBeRendered(this.props.data);
+    var offsetForRenderedRows = this.state.rowsToDisplay.toHideAbove * this.props.options.rowHeight;
+
+    // var tableStyle = {
+    //   transform: 'translate(0, ' + offsetForRenderedRows + 'px)',
+    //   OTransform: 'translate(0, ' + offsetForRenderedRows + 'px)',
+    //   msTransform: 'translate(0, ' + offsetForRenderedRows + 'px)',
+    //   MozTransform: 'translate(0, ' + offsetForRenderedRows + 'px)',
+    //   WebkitTransform: 'translate(0, ' + offsetForRenderedRows + 'px)'
+    // };
+
+    var tableStyle = {
+      position: 'relative',
+      top: offsetForRenderedRows
+    }
+
+    return (
+      <div style={{height: this.state.tableHeight}}>
+        <table ref="table" style={tableStyle}>
+          <thead>{
+            Object.keys(table[0]).map(this.createHeaderCell)
+          }</thead>
+          <tbody>
+            {table.map(this.createRow)}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 });
 
